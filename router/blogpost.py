4ce0492106c7b7f646afab22fd74b0e3bfda1128
rhen-ren <zerotwo.alanofren@outlook.com>
2026-06-23 20:20:37 +0800

    feat: implement GET endpoint for retrieving blog posts

--- router/blogpost.py
+++ router/blogpost.py
@@ -1,4 +1,11 @@
 from fastapi import FastAPI, APIRouter
+from schema.post import Post
+from testData import testData
 
 router = APIRouter(prefix="/blogpost")
+
+
+@router.get("/posts")
+def get_posts() -> Post:
+    return testData
 
