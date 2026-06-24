3e4d99c91c6f8bbaabb6ccba260240502a8f0aec
rhen-ren <zerotwo.alanofren@outlook.com>
2026-06-24 11:49:38 +0800

    feat: update database configuration and add initial module files

--- db.py
+++ db.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///students.db"
+DATABASE_URL = "sqlite:///blogposts.db"
 
 engine = create_engine(DATABASE_URL, echo=True)
-sessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+sessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
-base = declarative_base
+Base = declarative_base()
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI, APIRouter
 from router import blogpost
+from db import Base, engine
 import uvicorn
 
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
 app.include_router(blogpost.router)
 
