fd3a336ada885ce3c7a5a29513e11f306b9c9315
rhen-ren <zerotwo.alanofren@outlook.com>
2026-06-30 13:29:45 +0800

    feat: enhance post creation to handle dynamic tag and category creation

--- service/blogpostservices.py
+++ service/blogpostservices.py
@@ -1,47 +1,80 @@
 from dependency import run_db
 from schema.post import CreatePost, GetPost
-from model.post import Post, Category
+from model.post import Post
+from model.category import Category
+from model.tag import Tag
+from model.posttags import PostTags
 from sqlalchemy import select
 from sqlalchemy.orm import Session
+from fastapi import HTTPException
+from fastapi.responses import JSONResponse
 
 def create_post(post: CreatePost, db: Session):
-    # check if category exists
-    query = select(Category).where(Category.category_title == post.category)
-    is_category_exists: Category = db.execute(query).scalar_one_or_none()
+    try:
+        category = db.execute(select(Category).where(Category.category_title == post.category)).scalar_one_or_none()
+        tags = db.execute(select(Tag).where(Tag.tag_title.in_(post.tags))).scalars().all()
 
-    newPost = None
+        existingTagTitle = [tag.tag_title for tag in tags]
+        currentTagIds = [tag.id for tag in tags]
+        currentCategoryId = 0
 
-    if is_category_exists:
+        #if tagsExist
+        for postTag in post.tags:
+            if postTag not in existingTagTitle:
+                #create tag
+                newTag = Tag(
+                    tag_title = postTag
+                )
+                db.add(newTag)
+                db.commit()
+                db.refresh(newTag)
+                currentTagIds.append(newTag.id)
+
+        #if categoryExist
+        if category:
+            if post.category == category.category_title:
+                currentCategoryId = category.id
+        elif not category:
+                #create category
+                newCategory = Category(
+                    category_title = post.category
+                )
+                db.add(newCategory)
+                db.commit()
+                db.refresh(newCategory)
+                currentCategoryId = newCategory.id
+            
+        #create post
         newPost = Post(
-            title = post.title,
-            content = post.content,
-            category_id = is_category_exists.id
+           title = post.title,
+           content = post.content,
+           category_id = currentCategoryId
         )
+
         db.add(newPost)
         db.commit()
+        db.refresh(newPost)
 
-    elif not is_category_exists:
-        is_category_exists = Category(
-            category_title = post.category
-        )
-        db.add(is_category_exists)
-        db.commit()
-        db.refresh(is_category_exists)
-
-        newPost = Post(
-            title = post.title,
-            content = post.content,
-            category_id = is_category_exists.id
-        )
-        db.add(newPost)
+        #create post tag link
+        for tagId in currentTagIds:
+            newLink = PostTags(
+                post_id = newPost.id,
+                tag_id = tagId
+            )
+            db.add(newLink)
         db.commit()
 
-    return GetPost(
-        id = newPost.id,
-        content = newPost.content,
-        category = is_category_exists.category_title,
-        category_id = newPost.category_id
-    )
+        db.close()
+        return JSONResponse(
+            status_code=201,
+            content={"message": "Success"}
+        )
+
+    except Exception as e:
+        db.rollback()
+        raise e
+
+
 
 
 
