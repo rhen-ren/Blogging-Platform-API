29114cf2c7d4dc693b9d0f8989d16c68f0fc703d
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-01 11:16:43 +0800

    feat: update get_posts to retrieve posts from the database and fix function definitions

--- router/blogpost.py
+++ router/blogpost.py
@@ -11,8 +11,16 @@ router = APIRouter(prefix="/blogpost")
 
 
 @router.get("/posts")
-def get_posts() -> CreatePost:
-    return testData
+def get_posts(db: Session = Depends(run_db)) -> list[GetPost]:
+    return blogpostservices.get_all_posts(db)
+
+@router.get("/posts/{post_id}")
+def create_post(post_id: int, db:Session = Depends(run_db)):
+    return blogpostservices.get_post(post_id, db)
+
+@router.delete("/posts/{post_id}")
+def create_post(post_id: int, db:Session = Depends(run_db)):
+    return blogpostservices.delete_post(post_id, db)
 
 @router.post("/posts")
 def create_post(post: CreatePost, db: Session = Depends(run_db)):
