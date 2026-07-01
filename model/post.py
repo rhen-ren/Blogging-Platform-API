ee4560e917f709d65a4cc2677738519ebc064376
rhen-ren <zerotwo.alanofren@outlook.com>
2026-07-01 09:42:19 +0800

    refactor: add cascade delete to post_tags relationship in Post model

--- model/post.py
+++ model/post.py
@@ -18,6 +18,6 @@ class Post(Base):
     content: Mapped[str] = mapped_column(String(200))
     category_id: Mapped[int] = mapped_column(ForeignKey("category.id"))
     category: Mapped["Category"] = relationship("Category", back_populates="post")
-    post_tags: Mapped["PostTags"] = relationship("PostTags", back_populates= "post")
+    post_tags: Mapped["PostTags"] = relationship("PostTags", back_populates= "post", cascade="all, delete")
     createdAt: Mapped["datetime"] = mapped_column(DateTime, default=func.now())
     updatedAt: Mapped["datetime"] = mapped_column(DateTime, default=func.now(), onupdate=func.now())
